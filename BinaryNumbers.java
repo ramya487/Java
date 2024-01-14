c4a76d8268c4555a2722fe4b6d36740adcee3129
ramya487 <ramyas.aiml2022@citchennai.net>
2024-01-14 12:53:14 +0530

    generate binary representation using stack

--- BinaryNumbers.java
+++ BinaryNumbers.java
@@ -1,10 +1,36 @@
 /*
-Generate Binary Numbers from 1 to N using a Queue:
-Given a positive integer N, write a program to generate 
-all binary numbers from 1 to N using a Queue. */
-
+Generate Binary Numbers from 1 to N
+using stack
+*/
+import java.util.Scanner;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Stack;
 public class BinaryNumbers {
     public static void main(String[] args) {
-        
+        Scanner myObj = new Scanner(System.in);
+        int N;
+        System.out.print("Enter the value of N: ");
+        N = myObj.nextInt();
+        Map<Integer, String> map = new LinkedHashMap<Integer, String>();
+        Stack<Integer> s; // only declare a stack instance over here do not initialise
+        int element;
+        String string1;
+        for (int i = 1; i<=N; i++){
+            element = i;
+            string1 = "";
+            s = new Stack<Integer>(); // create a new instance of stack for each number in the range
+            while (element>0){
+                System.out.println(element);
+                s.push(element%2);
+                element = element/2;
+            }
+            while (!s.isEmpty()){
+                string1+=String.valueOf(s.pop());
+            }
+            map.put(i,string1);
+        }
+        System.out.println(map);
+        myObj.close();
     }
 }
